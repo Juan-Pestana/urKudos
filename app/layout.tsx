f791770667cd26b3a3382186658e04a3be21a4c7
Juan Pestana <juan.pestana82@gmail.com>
2022-12-04 22:57:31 +0100

    added logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './output.css'
 import { Montserrat } from '@next/font/google'
+import Image from 'next/image'
 
 const custom_font = Montserrat({
   subsets: ['latin'],
@@ -21,8 +22,16 @@ export default function RootLayout({
       </style>
       <head />
       <body className="bg-gray-800 text-gray-200 h-screen">
-        <header className="flex bg-gray-500 px-20 py-5">
-          <div>Hello</div>
+        <header className="flex bg-[#0c1d36] px-20 py-5 items-center">
+          <div className="w-40 object-cover">
+            <Image
+              className="w-full"
+              src="/images/imageLogo.png"
+              alt="Logo"
+              width={68}
+              height={16}
+            />
+          </div>
           <div className="flex-1"></div>
           <nav className="flex gap-6">
             <li className="list-none">About</li>
